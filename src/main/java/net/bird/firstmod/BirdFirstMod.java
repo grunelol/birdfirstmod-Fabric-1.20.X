2798d1d3799cc8499ac4707452f8d938e61b4f9c
ramitoma99 <154519701+ramitoma99@users.noreply.github.com>
2024-11-12 23:13:09 +0100

    This is my first change

--- src/main/java/net/bird/firstmod/BirdFirstMod.java
+++ src/main/java/net/bird/firstmod/BirdFirstMod.java
@@ -11,6 +11,7 @@ public class BirdFirstMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	//hello this is a change
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
