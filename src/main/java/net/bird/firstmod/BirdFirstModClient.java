d4336802052a70242362f29fcc409e391ad206c9
ramitoma99 <154519701+ramitoma99@users.noreply.github.com>
2024-11-12 23:08:53 +0100

    Initial commit

---
+++ src/main/java/net/bird/firstmod/BirdFirstModClient.java
@@ -0,0 +1,10 @@
+package net.bird.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BirdFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
